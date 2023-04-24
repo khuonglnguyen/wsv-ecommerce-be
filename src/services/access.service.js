18c8551bdf9f13bc2d998ae0a1e049af5d68340a
khuongnguyen0412 <lapankhuongnguyen@gmail.com>
2023-04-24 08:39:26 +0700

    JWT RSA

--- src/services/access.service.js
+++ src/services/access.service.js
@@ -1,6 +1,9 @@
 const shopModel = require("../models/shop.model");
 const bcrypt = require("bcrypt");
 const crypto = require("crypto");
+const KeyTokenService = require("./keyToken.service");
+const { createTokenPair } = require("../auth/authUtils");
+const { getInfoData } = require("../utils");
 
 const ROLE_SHOP = {
   SHOP: "SHOP",
@@ -13,6 +16,7 @@ class AccessService {
   static signUp = async ({ name, email, password }) => {
     try {
       const shop = await shopModel.findOne({ email }).lean();
+
       if (shop) {
         return {
           code: "xxx",
@@ -28,10 +32,63 @@ class AccessService {
         password: passwordHash,
         roles: [ROLE_SHOP.SHOP],
       });
+
       if (newShop) {
-        const { privateKey, publicKey } = crypto.generateKeyPairSync("rsa", {
-          modulusLength: 4096,
+        // const { privateKey, publicKey } = crypto.generateKeyPairSync("rsa", {
+        //   modulusLength: 4096,
+        //   publicKeyEncoding: {
+        //     type: "pkcs1",
+        //     format: "pem",
+        //   },
+        //   privateKeyEncoding: {
+        //     type: "pkcs1",
+        //     format: "pem",
+        //   },
+        // });
+
+        const privateKey = crypto.randomBytes(64).toString("hex");
+        const publicKey = crypto.randomBytes(64).toString("hex");
+
+        const keyStore = await KeyTokenService.createKeyToken({
+          userId: newShop._id,
+          publicKey,
+          privateKey,
         });
+
+        if (!keyStore) {
+          return {
+            code: "xxx",
+            message: error,
+            status: "keyStore error",
+          };
+        }
+
+        const tokens = await createTokenPair(
+          {
+            userId: newShop.id,
+            email,
+          },
+          publicKey,
+          privateKey
+        );
+
+        return {
+          code: 201,
+          metadata: {
+            shop: getInfoData({
+              fileds: ["_id", "name", "email"],
+              object: newShop,
+            }),
+            tokens,
+          },
+          status: "success",
+        };
+      } else {
+        return {
+          code: 200,
+          metadata: null,
+          status: "success",
+        };
       }
     } catch (error) {
       return {
---
+++ src/utils/index.js
@@ -0,0 +1,9 @@
+const _ = require("lodash");
+
+const getInfoData = ({ fileds = [], object = {} }) => {
+  return _.pick(object, fileds);
+};
+
+module.exports = {
+  getInfoData,
+};
