30066da0c0a2ea402801a6180c119b61128fe3dc
khuongnguyen0412 <lapankhuongnguyen@gmail.com>
2023-05-11 09:58:33 +0700

    Add product model

---
+++ src/models/product.model.js
@@ -0,0 +1,84 @@
+const { model, Schema } = require("mongoose"); // Erase if already required
+
+const DOCUMENT_NAME = "Product";
+const COLLECTION_NAME = "Products";
+
+// Declare the Schema of the Mongo model
+var productSchema = new Schema(
+  {
+    product_name: {
+      type: String,
+      required: true,
+    },
+    product_thumb: {
+      type: String,
+      required: true,
+    },
+    product_description: {
+      type: String,
+      required: true,
+    },
+    product_price: {
+      type: Number,
+      required: true,
+    },
+    product_quantity: {
+      type: Number,
+      required: true,
+    },
+    product_type: {
+      type: String,
+      required: true,
+      enum: ["Electionics", "Clothing", "Furniture"],
+    },
+    product_shop: {
+      type: Schema.Types.ObjectId,
+      ref: "Shop",
+    },
+    product_attributes: {
+      type: Schema.Types.Mixed,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    collection: COLLECTION_NAME,
+  }
+);
+
+const clothingSchema = new Schema(
+  {
+    brand: {
+      type: String,
+      required: true,
+    },
+    size: String,
+    material: String,
+  },
+  {
+    collection: "clothes",
+    timestamps: true,
+  }
+);
+
+const electronicSchema = new Schema(
+  {
+    manufacturer: {
+      type: String,
+      required: true,
+    },
+    model: String,
+    color: String,
+  },
+  {
+    collection: "electronics",
+    timestamps: true,
+  }
+);
+
+//Export the model
+module.exports = {
+  product: model(DOCUMENT_NAME, productSchema),
+  clothing: model("Clothing", clothingSchema),
+  electronic: model("Electronics", electronicSchema),
+};
